5b716a508d95510972b716166cb2b300730ca29a
Hank Beaver <hbeaver+hank@gmail.com>
2013-02-19 11:30:18 -0500

    fixed failing spec

--- spec/web_spec.rb
+++ spec/web_spec.rb
@@ -10,19 +10,6 @@ describe Web do
     get "/"
     last_response.ok?.must.be_true
   end
-  describe "#try_showing_data" do
-    it "should not show anything if methods are nil" do 
-    MetricsInfo.configure do |c|
-      c.register_app_info 'app_name', 'Readouts'
-      c.register_app_info 'file_root', APP_ROOT
-      c.register_app_info 'github_base_url', 'https://github.com/blasterpal/readouts'
-    end
-    @metrics_info = MetricsInfo.new
-    get '/'
-    app.try_showing_data(@metrics_info,:app_info,:foo).must.equal ''
-    end
-
-  end
 
 end
 
